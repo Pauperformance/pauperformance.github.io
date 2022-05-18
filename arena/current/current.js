50101d0bd73189105316c06de63af7fbfedf2cfd
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2022-05-18 10:26:21 +0200

    Updated plugins for Arena

---
+++ arena/current/current.js
@@ -0,0 +1,11 @@
+const currDeckId = '4822197'
+
+function redirectToCurrentDeck() {
+    const url = 'https://www.mtggoldfish.com/deck/' + currDeckId;
+    document.write('<meta http-equiv="refresh" content="0; URL=' + url + '" />');
+}
+
+function redirectToCurrentPopout() {
+    const url = 'https://www.mtggoldfish.com/deck/popout?id=' + currDeckId;
+    document.write('<meta http-equiv="refresh" content="0; URL=' + url + '" />');
+}
---
+++ arena/current/popout.md
@@ -0,0 +1,4 @@
+<script src="current.js"></script>
+
+<body onload="redirectToCurrentPopout()">
+</body>
